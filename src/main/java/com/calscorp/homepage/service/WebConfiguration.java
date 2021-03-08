64ceff5a2c6d19d75fff309bdfa5dbf349175161
kantabest <kantabest@gmail.com>
2021-03-08 13:22:44 +0900

    리소스 핸들러 추가

--- src/main/java/com/calscorp/homepage/service/WebConfiguration.java
+++ src/main/java/com/calscorp/homepage/service/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.calscorp.homepage.service;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,15 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/views/index/KR");
     }
+
+    @Override
+    // 연결된 다운로드 파일은 네트워크 드라이브로 연결되어 있어 해당 폴더를 리소스 핸들러에 추가한다.
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/link/sdc112/**")                
+                .addResourceLocations("file:///x:/"); 
+
+
+    }
+
     
 }
