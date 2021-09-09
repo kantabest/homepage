89cbf29a6f6206ff69b02084418deb5ac8f7d434
kantabest <kantabest@gmail.com>
2021-09-09 13:20:43 +0900

    21.09.09

--- src/main/java/com/calscorps/homepage/service/WebConfiguration.java
+++ src/main/java/com/calscorps/homepage/service/WebConfiguration.java
@@ -27,44 +27,44 @@ public class WebConfiguration implements WebMvcConfigurer {
     // 연결된 다운로드 파일은 네트워크 드라이브로 연결되어 있어 해당 폴더를 리소스 핸들러에 추가한다.
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/link/sdc112/**")                
-                .addResourceLocations("file:///x:/"); 
-
-
+                .addResourceLocations("file:///C:\\inetpub\\wwwroot\\EMAX_ERP\\Ftp_File\\SDC112/");         
+                // .addResourceLocations("file:///x:/");                 
+                
     }
 
 
-    @Value("${server.port}")
-    private int serverPort;
+    // @Value("${server.port}")
+    // private int serverPort;
    
-    private int httpPort = 80;
+    // private int httpPort = 80;
 
     
-    @Bean
-    public ServletWebServerFactory servletContainer() {
+    // @Bean
+    // public ServletWebServerFactory servletContainer() {
 
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(createSslConnector());
-        return tomcat;
-    }
+    //     TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+    //         @Override
+    //         protected void postProcessContext(Context context) {
+    //             SecurityConstraint securityConstraint = new SecurityConstraint();
+    //             securityConstraint.setUserConstraint("CONFIDENTIAL");
+    //             SecurityCollection collection = new SecurityCollection();
+    //             collection.addPattern("/*");
+    //             securityConstraint.addCollection(collection);
+    //             context.addConstraint(securityConstraint);
+    //         }
+    //     };
+    //     tomcat.addAdditionalTomcatConnectors(createSslConnector());
+    //     return tomcat;
+    // }
 
-    private Connector createSslConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setSecure(false);
-        connector.setPort(httpPort);
-        connector.setRedirectPort(serverPort);
-        return connector;
-    }
+    // private Connector createSslConnector() {
+    //     Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    //     connector.setScheme("http");
+    //     connector.setSecure(false);
+    //     connector.setPort(httpPort);
+    //     connector.setRedirectPort(serverPort);
+    //     return connector;
+    // }
 
     
 }
